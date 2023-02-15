98d47647ff88788543e503dad97ba2818fd65193
darkwarrior303 <ysdeveloper1998@gmail.com>
2023-02-15 23:43:37 +0530

    changes updated.

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -85,7 +85,7 @@ const Hero = () => {
                         <Image src="https://images.unsplash.com/photo-1523049820105-c2e73204bac1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG5ldyUyMHplYWxhbmR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" width={350} height={60} alt="Logo" />
                     </div>
                     <div className="slide">
-                        <Image src="https://images.unsplash.com/photo-1612975702762-7cc2733b3e75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fHRpZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2500&q=90" width={2500} height={60} alt="Logo" />
+                        <Image src="https://images.unsplash.com/photo-1612975702762-7cc2733b3e75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fHRpZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2500&q=90" priority width={2500} height={60} alt="Logo" />
                     </div>
                     <div className="slide">
                         <Image src="https://images.unsplash.com/photo-1515268064940-5150b7c29f35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdhbGxwYXBlcnMlMjA0a3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=80" width={350} height={60} alt="Logo" />
